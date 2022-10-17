2dac8a5e55b95b694d0841696bec337821b5a1fa
satnamkhowal <satnamkhowal@gmail.com>
2022-10-17 19:41:35 +0530

    privacy and policy page added
    privacy and policy page added

--- includes/footer.php
+++ includes/footer.php
@@ -1,62 +1,63 @@
        <!-- Footer Start -->
        <footer id="rs-footer" class="rs-footer">
-            <div class="footer-top">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-lg-6 col-md-12 col-sm-12 footer-widget">
-                            <div class="footer-logo mb-30">
-                                <a href="index.php"><img src="assets/images/logo-dark.png" alt=""></a>
-                            </div>
-                              <div class="textwidget pb-30"><p>Groot Software is a go-to-destination for all your web development, software development and any other digital development task. We also provide digital marketing services for your business.</p>
-                              </div>
-                              <ul class="footer-social md-mb-30">  
-                                  <li><a href="https://www.facebook.com/GrootSoftware/" target="_blank"><i class="fa fa-facebook"></i></a></li>
-                                            <li><a href="https://twitter.com/grootsoftware" target="_blank"><i class="fa fa-twitter"></i></a></li>
-                                            <li><a href="https://github.com/grootsoftware" target="_blank"><i class="fa fa-git"></i></a></li>
-                                            <li><a href="https://www.instagram.com/grootsoftware/" target="_blank"><i class="fa fa-instagram"></i></a></li>
-                                            <li><a href="https://www.linkedin.com/in/grootsoftware/" target="_blank"><i class="fa fa-linkedin"></i></a></li>
-                                            <li><a href="https://stackoverflow.com/c/grootsoftware/" target="_blank"><i class="fa fa-stack-overflow"></i></a></li>                                                         
-                              </ul>
-                        </div>
-                        <div class="col-lg-3 col-md-12 col-sm-12 pl-45 md-pl-15 md-mb-30">
-                            <h3 class="widget-title">IT Services</h3>
-                            <ul class="site-map">
-                                <li><a href="software-development.php">Software Development</a></li>
-                                <li><a href="web-development-services.php">Web Development</a></li>
-                                <li><a href="analytic-solutions.php">Analytic Solutions</a></li>
-                                <li><a href="cloud-and-devOps-services.php">Cloud and DevOps</a></li>
-                                <li><a href="product-design-services.php">Product Design</a></li>
-                            </ul>
-                        </div>
-                        <div class="col-lg-3 col-md-12 col-sm-12 md-mb-30">
-                            <h3 class="widget-title">Contact Info</h3>
-                            <ul class="address-widget">
-                                <li>
-                                    <i class="flaticon-location"></i>
-                                    <div class="desc"> 73/34, Paramhans Marg, Mansarovar, Jaipur </div>
-                                </li>
-                                <li>
-                                    <i class="flaticon-call"></i>
-                                    <div class="desc">
+           <div class="footer-top">
+               <div class="container">
+                   <div class="row">
+                       <div class="col-lg-6 col-md-12 col-sm-12 footer-widget">
+                           <div class="footer-logo mb-30">
+                               <a href="index.php"><img src="assets/images/logo-dark.png" alt=""></a>
+                           </div>
+                           <div class="textwidget pb-30">
+                               <p>Groot Software is a go-to-destination for all your web development, software development and any other digital development task. We also provide digital marketing services for your business.</p>
+                           </div>
+                           <ul class="footer-social md-mb-30">
+                               <li><a href="https://www.facebook.com/GrootSoftware/" target="_blank"><i class="fa fa-facebook"></i></a></li>
+                               <li><a href="https://twitter.com/grootsoftware" target="_blank"><i class="fa fa-twitter"></i></a></li>
+                               <li><a href="https://github.com/grootsoftware" target="_blank"><i class="fa fa-git"></i></a></li>
+                               <li><a href="https://www.instagram.com/grootsoftware/" target="_blank"><i class="fa fa-instagram"></i></a></li>
+                               <li><a href="https://www.linkedin.com/in/grootsoftware/" target="_blank"><i class="fa fa-linkedin"></i></a></li>
+                               <li><a href="https://stackoverflow.com/c/grootsoftware/" target="_blank"><i class="fa fa-stack-overflow"></i></a></li>
+                           </ul>
+                       </div>
+                       <div class="col-lg-3 col-md-12 col-sm-12 pl-45 md-pl-15 md-mb-30">
+                           <h3 class="widget-title">IT Services</h3>
+                           <ul class="site-map">
+                               <li><a href="software-development.php">Software Development</a></li>
+                               <li><a href="web-development-services.php">Web Development</a></li>
+                               <li><a href="analytic-solutions.php">Analytic Solutions</a></li>
+                               <li><a href="cloud-and-devOps-services.php">Cloud and DevOps</a></li>
+                               <li><a href="product-design-services.php">Product Design</a></li>
+                           </ul>
+                       </div>
+                       <div class="col-lg-3 col-md-12 col-sm-12 md-mb-30">
+                           <h3 class="widget-title">Contact Info</h3>
+                           <ul class="address-widget">
+                               <li>
+                                   <i class="flaticon-location"></i>
+                                   <div class="desc"> 73/34, Paramhans Marg, Mansarovar, Jaipur </div>
+                               </li>
+                               <li>
+                                   <i class="flaticon-call"></i>
+                                   <div class="desc">
                                        <a href="tel:+917790847091">+91-7790847091</a>
-                                    </div>
-                                </li>
-                                <li>
-                                    <i class="flaticon-email"></i>
-                                    <div class="desc">
-                                        <a href="mailto:info@grootsoftware.com"> info@grootsoftware.com</a>
-                                    </div>
-                                </li>
-                                <li>
-                                    <i class="flaticon-clock-1"></i>
-                                    <div class="desc">
-                                        Opening Hours: 8:00AM - 9:00PM   
-                                    </div>
-                                </li>
-                            </ul>
-                        </div>
-                        <!-- News Latter -->
-                        <!-- <div class="col-lg-3 col-md-12 col-sm-12">
+                                   </div>
+                               </li>
+                               <li>
+                                   <i class="flaticon-email"></i>
+                                   <div class="desc">
+                                       <a href="mailto:info@grootsoftware.com"> info@grootsoftware.com</a>
+                                   </div>
+                               </li>
+                               <li>
+                                   <i class="flaticon-clock-1"></i>
+                                   <div class="desc">
+                                       Opening Hours: 8:00AM - 9:00PM
+                                   </div>
+                               </li>
+                           </ul>
+                       </div>
+                       <!-- News Latter -->
+                       <!-- <div class="col-lg-3 col-md-12 col-sm-12">
                             <h3 class="widget-title">Newsletter</h3>
                             <p class="widget-desc">We denounce with righteous and in and dislike men who are so beguiled and demo realized.</p>
                             <p>
@@ -65,95 +66,96 @@
                                 <i class="flaticon-send"></i>
                             </p>
                         </div> -->
-                        <!-- News Latter -->
-                    </div>
-                </div>
-            </div>
-            <div class="footer-bottom">
-                <div class="container">                    
-                    <div class="row y-middle">
-                        <div class="col-lg-6 text-right md-mb-10 order-last">
-                            <ul class="copy-right-menu">
-                                <li><a href="index.php">Home</a></li>
-                                <li><a href="about.php">About</a></li>
-                                <li><a href="blog.php">Blog</a></li>
-                                <li><a href="shop.php">Shop</a></li>
-                                <li><a href="faq.php">FAQs</a></li>
-                            </ul>
-                        </div>
-                        <div class="col-lg-6">
-                            <div class="copyright">
-                                <p>&copy; 2021 All Rights Reserved. Developed By <a href="http://grootsoftware.com/">GrootSoftware Pvt. Ltd.</a></p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </footer>
-        <!-- Footer End -->
+                       <!-- News Latter -->
+                   </div>
+               </div>
+           </div>
+           <div class="footer-bottom">
+               <div class="container">
+                   <div class="row y-middle">
+                       <div class="col-lg-6 text-right md-mb-10 order-last">
+                           <ul class="copy-right-menu">
+                               <li><a href="index.php">Home</a></li>
+                               <li><a href="about.php">About</a></li>
+                               <li><a href="blog.php">Blog</a></li>
+                               <li><a href="shop.php">Shop</a></li>
+                               <li><a href="faq.php">FAQs</a></li>
+                               <li><a href="Privacy_Policy_For_Groot_Software.php">Privacy and Policy</a></li>
+                           </ul>
+                       </div>
+                       <div class="col-lg-6">
+                           <div class="copyright">
+                               <p>&copy; 2021 All Rights Reserved. Developed By <a href="http://grootsoftware.com/">GrootSoftware Pvt. Ltd.</a></p>
+                           </div>
+                       </div>
+                   </div>
+               </div>
+           </div>
+       </footer>
+       <!-- Footer End -->
 
-        <!-- start scrollUp  -->
-        <div id="scrollUp" class="orange-color">
-            <i class="fa fa-angle-up"></i>
-        </div>
-        <!-- End scrollUp  -->
+       <!-- start scrollUp  -->
+       <div id="scrollUp" class="orange-color">
+           <i class="fa fa-angle-up"></i>
+       </div>
+       <!-- End scrollUp  -->
 
-        <!-- Search Modal Start -->
-        <div aria-hidden="true" class="modal fade search-modal" role="dialog" tabindex="-1">
-            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-              <span class="flaticon-cross"></span>
-            </button>
-            <div class="modal-dialog modal-dialog-centered">
-                <div class="modal-content">
-                    <div class="search-block clearfix">
-                        <form>
-                            <div class="form-group">
-                                <input class="form-control" placeholder="Search Here..." type="text">
-                            </div>
-                        </form>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <!-- Search Modal End -->
+       <!-- Search Modal Start -->
+       <div aria-hidden="true" class="modal fade search-modal" role="dialog" tabindex="-1">
+           <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+               <span class="flaticon-cross"></span>
+           </button>
+           <div class="modal-dialog modal-dialog-centered">
+               <div class="modal-content">
+                   <div class="search-block clearfix">
+                       <form>
+                           <div class="form-group">
+                               <input class="form-control" placeholder="Search Here..." type="text">
+                           </div>
+                       </form>
+                   </div>
+               </div>
+           </div>
+       </div>
+       <!-- Search Modal End -->
 
-         <!-- modernizr js -->
-         <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
-         <script src="assets/js/script.js"></script>
-        <!-- jquery latest version -->
-        <script src="assets/js/jquery.min.js"></script>
-        <!-- Bootstrap v4.4.1 js -->
-        <script src="assets/js/bootstrap.min.js"></script>
-        <!-- Menu js -->
-        <!-- <script src="assets/js/rsmenu-main.js"></script>  -->
-        <!-- op nav js -->
-        <script src="assets/js/jquery.nav.js"></script>
-        <!-- Time Circle js -->
-        <script src="assets/js/time-circle.js"></script>
-        <!-- owl.carousel js -->
-        <script src="assets/js/owl.carousel.min.js"></script>
-        <!-- wow js -->
-        <script src="assets/js/wow.min.js"></script>
-        <!-- Skill bar js -->
-        <script src="assets/js/skill.bars.jquery.js"></script>
-        <script src="assets/js/jquery.counterup.min.js"></script> 
-         <!-- counter top js -->
-        <script src="assets/js/waypoints.min.js"></script>
-        <!-- swiper js -->
-        <script src="assets/js/swiper.min.js"></script>   
-        <!-- particles js -->
-        <script src="assets/js/particles.min.js"></script>  
-        <!-- magnific popup js -->
-        <script src="assets/js/jquery.magnific-popup.min.js"></script>      
-        <script src="assets/js/jquery.easypiechart.min.js"></script>      
-        <!-- plugins js -->
-        <script src="assets/js/plugins.js"></script>
-        <!-- pointer js -->
-        <script src="assets/js/pointer.js"></script>
-        <!-- contact form js -->
-        <script src="assets/js/contact.form.js"></script>
-        <!-- main js -->
-        <script src="assets/js/main.js"></script>
-    </body>
+       <!-- modernizr js -->
+       <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+       <script src="assets/js/script.js"></script>
+       <!-- jquery latest version -->
+       <script src="assets/js/jquery.min.js"></script>
+       <!-- Bootstrap v4.4.1 js -->
+       <script src="assets/js/bootstrap.min.js"></script>
+       <!-- Menu js -->
+       <!-- <script src="assets/js/rsmenu-main.js"></script>  -->
+       <!-- op nav js -->
+       <script src="assets/js/jquery.nav.js"></script>
+       <!-- Time Circle js -->
+       <script src="assets/js/time-circle.js"></script>
+       <!-- owl.carousel js -->
+       <script src="assets/js/owl.carousel.min.js"></script>
+       <!-- wow js -->
+       <script src="assets/js/wow.min.js"></script>
+       <!-- Skill bar js -->
+       <script src="assets/js/skill.bars.jquery.js"></script>
+       <script src="assets/js/jquery.counterup.min.js"></script>
+       <!-- counter top js -->
+       <script src="assets/js/waypoints.min.js"></script>
+       <!-- swiper js -->
+       <script src="assets/js/swiper.min.js"></script>
+       <!-- particles js -->
+       <script src="assets/js/particles.min.js"></script>
+       <!-- magnific popup js -->
+       <script src="assets/js/jquery.magnific-popup.min.js"></script>
+       <script src="assets/js/jquery.easypiechart.min.js"></script>
+       <!-- plugins js -->
+       <script src="assets/js/plugins.js"></script>
+       <!-- pointer js -->
+       <script src="assets/js/pointer.js"></script>
+       <!-- contact form js -->
+       <script src="assets/js/contact.form.js"></script>
+       <!-- main js -->
+       <script src="assets/js/main.js"></script>
+       </body>
 
-</html>
+       </html>
